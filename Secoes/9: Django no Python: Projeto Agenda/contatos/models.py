315feab3ef1843c1bf22869e4e46e5875b5ac056
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-21 18:15:17 -0300

    178: admin page

--- Secoes/9: Django no Python: Projeto Agenda/contatos/models.py
+++ Secoes/9: Django no Python: Projeto Agenda/contatos/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.nome
 
 
 class Contato(models.Model):
@@ -14,3 +19,6 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey('Categoria', on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
