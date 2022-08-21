42ff2b8d88177612c44fff8257bccf90c6847279
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-21 18:25:47 -0300

    179: view with contacts dynamically added

--- Secoes/9: Django no Python: Projeto Agenda/contatos/views.py
+++ Secoes/9: Django no Python: Projeto Agenda/contatos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Contato
 
+
 def index(request):
-    return render(request, 'contatos/index.html', {'nome': 'Jefferson Michael'})
+    contatos = Contato.objects.all()
+    return render(request, 'contatos/index.html', {'contatos': contatos})
