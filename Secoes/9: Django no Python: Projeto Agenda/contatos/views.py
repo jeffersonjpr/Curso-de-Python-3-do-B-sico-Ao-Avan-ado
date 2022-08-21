13dcad218eafa8033544815507c8519aeca54ed3
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-21 18:38:29 -0300

    180: unico contato

--- Secoes/9: Django no Python: Projeto Agenda/contatos/views.py
+++ Secoes/9: Django no Python: Projeto Agenda/contatos/views.py
@@ -5,3 +5,7 @@ from .models import Contato
 def index(request):
     contatos = Contato.objects.all()
     return render(request, 'contatos/index.html', {'contatos': contatos})
+
+def contact(request, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+    return render(request, 'contatos/contact.html', {'contato': contato})
