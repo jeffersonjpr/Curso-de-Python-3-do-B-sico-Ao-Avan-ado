0774f13ef5710a90dd08582268d9f3e0faa9065b
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-23 08:39:14 -0300

    182: condicional

--- Secoes/9: Django no Python: Projeto Agenda/contatos/admin.py
+++ Secoes/9: Django no Python: Projeto Agenda/contatos/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from .models import Categoria, Contato
 
+
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
-    list_display_links = ('id','nome')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone',
+                    'email', 'data_criacao', 'categoria', 'mostrar')
+    list_display_links = ('id', 'nome')
     list_filter = ('categoria', )
     list_per_page = 10
     search_fields = ('nome', 'sobrenome', 'telefone', 'email')
+    list_editable = ('mostrar', 'telefone')
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
