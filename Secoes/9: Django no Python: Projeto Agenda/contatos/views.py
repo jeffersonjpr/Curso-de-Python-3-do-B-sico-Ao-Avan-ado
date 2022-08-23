4685092de149adf4f7ce41345c00894ba6cdfd7f
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-22 21:56:33 -0300

    181: erro 404 cru

--- Secoes/9: Django no Python: Projeto Agenda/contatos/views.py
+++ Secoes/9: Django no Python: Projeto Agenda/contatos/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Contato
+from django.http import Http404
 
 
 def index(request):
     contatos = Contato.objects.all()
     return render(request, 'contatos/index.html', {'contatos': contatos})
 
+
 def contact(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
-    return render(request, 'contatos/contact.html', {'contato': contato})
+    try:
+        contato = Contato.objects.get(id=contato_id)
+        return render(request, 'contatos/contact.html', {
+            'contato': contato
+        })
+    except Contato.DoesNotExist as e:
+        raise Http404('Contato não encontrado')
