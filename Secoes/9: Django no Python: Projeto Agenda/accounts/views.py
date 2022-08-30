eb3be66400857c9558ddddf4cc1c3ca6fbac264f
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-30 08:02:40 -0300

    191: fianlizando base cadastro de usuários

--- Secoes/9: Django no Python: Projeto Agenda/accounts/views.py
+++ Secoes/9: Django no Python: Projeto Agenda/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 # Create your views here.
 def login(request):
@@ -8,6 +9,7 @@ def logout(request):
     return render(request, 'accounts/logout.html')
 
 def register(request):
+    print(request.POST)
     return render(request, 'accounts/register.html')
 
 def dashboard(request):
